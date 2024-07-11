c7b90621f02733cbc4cff70e70728b16a4959bf0
shareestark <shareegstark@gmail.com>
2024-07-10 22:57:47 -0400

    Third commit, showing up to 12 quotes in UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Loader } from "react-feather";
+import Quotes from "./components/quotes/Quotes";
 import "./App.css";
 
+
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(false);
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
+  const fetchQuotes = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(quotesUrl);
+      const data = await response.json();
+      setQuotes(data);
+    } catch (error) {
+      console.error("There was an error fetching the quotes: ", error);
+    } setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
-      <main></main>
+      <main>
+        {loading ? (
+          <Loader />
+        ) : (
+          <Quotes quotes={quotes} />
+        )}
+      </main>
       <Footer />
     </div>
   );
 }
+
 export default App;
