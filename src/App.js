456488f89bac8328ea36d01e63ae17e6eb087a4e
shareestark <shareegstark@gmail.com>
2024-08-13 14:05:59 -0400

    Adding pop-up message when adding favorite quotes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Message from "./components/Message";
 import { Loader } from "react-feather";
 import Quotes from "./components/quotes/Quotes";
 import FavoriteQuotes from "./components/quotes/FavoriteQuotes";
@@ -12,10 +13,11 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState("All"); 
   const [favoriteQuotes, setFavoriteQuotes] = useState([]);
+  const [messageText, setMessageText] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
   const maxFaves = 3;
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
-
   const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
 
   const fetchQuotes = async () => {
@@ -47,14 +49,21 @@ function App() {
     const alreadyFavorite = favoriteQuotes.find((favorite) => favorite.id === selectedQuote.id);
 
     if (alreadyFavorite) {
-      console.log("You already favorited this quote!");
+      setMessageText("You already favorited this quote!");
+      setShowMessage(true);
     } else if (favoriteQuotes.length < maxFaves) {
       setFavoriteQuotes([...favoriteQuotes, selectedQuote]);
-      console.log("Added to Favorites");
+      setMessageText("Added to Favorites");
+      setShowMessage(true);
     } else {
-      console.log("Max number of quotes reached. Please delete one to add a new one.")
+      setMessageText("Max number of quotes reached. Please delete one to add a new one.")
+      setShowMessage(true);
     }
   };
+
+  const removeMessage = () => {
+    setShowMessage(false);
+  }
 
   const removeFromFavorites = (quoteId) => {
     const updatedFavorites = favoriteQuotes.filter((quote) => quote.id !== quoteId);
@@ -64,6 +73,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      { showMessage && <Message messageText={messageText} removeMessage={removeMessage}/>}
       <main>
         <FavoriteQuotes favoriteQuotes={favoriteQuotes} maxFaves={maxFaves} removeFromFavorites={removeFromFavorites}/>
         {loading ? (
---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Message = ({messageText, removeMessage}) => {
+    return (
+        <div className="message">
+            <p>{messageText}</p>
+            <span className="close-message" onClick={removeMessage}>X</span>
+        </div>
+    );
+};
+
+export default Message;
